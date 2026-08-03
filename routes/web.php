2a542e62c236e0e6fa01831307b69e6b98b45db9
Nguyen Dai Viet Hoang <h3m1n1@gmail.com>
2026-08-04 00:28:34 +0700

    feat(ch06): define resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,22 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
+
+Route::resource('users', UserController::class);
+
+Route::controller(TaskController::class)
+    ->prefix('tasks')
+    ->name('tasks.')
+    ->group(function (): void {
+        Route::get('/', 'index')->name('index');
+        Route::get('/create', 'create')->name('create');
+        Route::post('/', 'store')->name('store');
+        Route::get('/{task}', 'show')->name('show');
+        Route::get('/{task}/edit', 'edit')->name('edit');
+        Route::match(['put', 'patch'], '/{task}', 'update')->name('update');
+        Route::delete('/{task}', 'destroy')->name('destroy');
+    });
