516b9e295bf26651bba089c1d5156a05400cf69a
Nguyen Dai Viet Hoang <h3m1n1@gmail.com>
2026-08-04 03:10:19 +0700

    feat(ch08): add quicktask authentication

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+
         $middleware->web(append: [
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,44 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+})->name('home');
 
-Route::resource('users', UserController::class)->middleware('super_admin');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::controller(TaskController::class)
-    ->prefix('tasks')
-    ->name('tasks.')
-    ->group(function (): void {
-        Route::get('/', 'index')->name('index');
-        Route::get('/create', 'create')->name('create');
-        Route::post('/', 'store')->name('store');
-        Route::get('/{task}', 'show')->name('show');
-        Route::get('/{task}/edit', 'edit')->name('edit');
-        Route::match(['put', 'patch'], '/{task}', 'update')->name('update');
-        Route::delete('/{task}', 'destroy')->name('destroy');
-    });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('users', UserController::class)->middleware('super_admin');
+
+    Route::controller(TaskController::class)
+        ->prefix('tasks')
+        ->name('tasks.')
+        ->group(function (): void {
+            Route::get('/', 'index')->name('index');
+            Route::get('/create', 'create')->name('create');
+            Route::post('/', 'store')->name('store');
+            Route::get('/{task}', 'show')->name('show');
+            Route::get('/{task}/edit', 'edit')->name('edit');
+            Route::match(['put', 'patch'], '/{task}', 'update')->name('update');
+            Route::delete('/{task}', 'destroy')->name('destroy');
+        });
+});
+
+require __DIR__.'/auth.php';
