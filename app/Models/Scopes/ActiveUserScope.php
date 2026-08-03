7a64dcf7979e7266582793bf55fbbc145384a407
Nguyen Dai Viet Hoang <h3m1n1@gmail.com>
2026-08-04 03:35:06 +0700

    style(ch04): fix pint formatting issue with fully qualified type

--- app/Models/Scopes/ActiveUserScope.php
+++ app/Models/Scopes/ActiveUserScope.php
@@ -2,12 +2,13 @@
 
 namespace App\Models\Scopes;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 
 /**
- * @implements Scope<\App\Models\User>
+ * @implements Scope<User>
  */
 class ActiveUserScope implements Scope
 {
